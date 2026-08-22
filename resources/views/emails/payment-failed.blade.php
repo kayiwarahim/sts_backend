9b342a747d44a5c55f9b883c0b4a4ea404000fd8
Kayiwa Rahim <kayiwarahim@gmail.com>
2026-08-23 01:46:50 +0300

    updated colors

--- resources/views/emails/payment-failed.blade.php
+++ resources/views/emails/payment-failed.blade.php
@@ -11,7 +11,7 @@
         <table width="100%" cellpadding="0" cellspacing="0" style=" padding:30px 15px; ">
             <tr>
                 <td align="center">
-                    <table width="100%" cellpadding="0" cellspacing="0" style=" max-width:600px; background:#ffffff; border:1px solid #e2e8f0; border-radius:12px; ">
+                    <table width="100%" cellpadding="0" cellspacing="0" style=" max-width:600px; background:#ffffff; border-radius:12px; overflow:hidden; border:1px solid #e2e8f0; ">
                         <tr>
                             <td
                                 style=" padding:24px; background:#991b1b; color:white; " >
--- resources/views/emails/payment-successful.blade.php
+++ resources/views/emails/payment-successful.blade.php
@@ -30,7 +30,7 @@
                     <td align="center">
                         <table width="100%" cellpadding="0" cellspacing="0" style=" max-width:600px; background:#ffffff; border-radius:12px; overflow:hidden; border:1px solid #e2e8f0; ">
                             <tr>
-                                <td style=" padding:24px; background:#0f172a; color:white; ">
+                                <td style=" padding:24px; background:#18d208; color:white; ">
                                     <h2 style="margin:0;">
                                         Water Payment Successful
                                     </h2>

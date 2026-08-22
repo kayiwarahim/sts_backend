e62550dd873f80b123b0185bd77e6fcb1d0478e0
Kayiwa Rahim <kayiwarahim@gmail.com>
2026-08-23 01:39:20 +0300

    redirect api

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return redirect('/up');
+});
 
 /*
 |--------------------------------------------------------------------------
